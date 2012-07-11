b734b9cf50afe5b19b926242b068a1f5901e1ba3
Ca1ne <Ca1nexda@gmail.com>
2012-07-11 00:30:42 -0400

    redo avs

--- arch/arm/mach-msm/avs.h
+++ arch/arm/mach-msm/avs.h
@@ -31,7 +31,7 @@
 
 #define VOLTAGE_MIN  CONFIG_CPU_FREQ_VDD_LEVELS_MIN /* mV */
 #define VOLTAGE_MAX  CONFIG_CPU_FREQ_VDD_LEVELS_MAX
-#define VOLTAGE_MIN_START  950 // Minimum value to start off with
+#define VOLTAGE_MIN_START  925 // Minimum value to start off with
 #define VOLTAGE_STEP 25
 
 int __init avs_init(int (*set_vdd)(int), u32 freq_cnt, u32 freq_idx);
@@ -40,18 +40,47 @@ void __exit avs_exit(void);
 int avs_adjust_freq(u32 freq_index, int begin);
 
 /* Routines exported from avs_hw.S */
+#ifdef CONFIG_MSM_CPU_AVS
 u32 avs_test_delays(void);
+#else
+static inline u32 avs_test_delays(void)
+{ return 0; }
+#endif
+
+#ifdef CONFIG_MSM_AVS_HW
 u32 avs_reset_delays(u32 avsdscr);
 u32 avs_get_avscsr(void);
 u32 avs_get_avsdscr(void);
 u32 avs_get_tscsr(void);
-void     avs_set_tscsr(u32 to_tscsr);
-
-#ifdef CONFIG_MSM_CPU_AVS_DEBUG
-#define AVSDEBUG(x...) pr_info("AVS: " x);
+void avs_set_tscsr(u32 to_tscsr);
+void avs_disable(void);
 #else
-#define AVSDEBUG(...)
+static inline u32 avs_reset_delays(u32 avsdscr)
+{ return 0; }
+static inline u32 avs_get_avscsr(void)
+{ return 0; }
+static inline u32 avs_get_avsdscr(void)
+{ return 0; }
+static inline u32 avs_get_tscsr(void)
+{ return 0; }
+static inline void avs_set_tscsr(u32 to_tscsr) {}
+static inline void avs_disable(void) {}
 #endif
+
+//#define AVSDEBUG(x...) pr_info("AVS: " x);
+#define AVSDEBUG(...)
+
+#define AVS_DISABLE(cpu) do {			\
+		if (get_cpu() == (cpu))		\
+			avs_disable();		\
+		put_cpu();			\
+	} while (0);
+
+#define AVS_ENABLE(cpu, x) do {			\
+		if (get_cpu() == (cpu))		\
+			avs_reset_delays((x));	\
+		put_cpu();			\
+	} while (0);
 
 #endif /* AVS_H */
 
